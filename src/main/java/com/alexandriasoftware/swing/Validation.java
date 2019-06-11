fa5118e76483fa73612d168263ae87cd8f54875f
Randall Wood <297232+rhwood@users.noreply.github.com>
2019-06-11 06:35:33 -0400

    doc: Javadocs for Validation

--- src/main/java/com/alexandriasoftware/swing/Validation.java
+++ src/main/java/com/alexandriasoftware/swing/Validation.java
@@ -19,6 +19,8 @@ import java.awt.Color;
 import java.awt.Font;
 
 /**
+ * A Validation is the combination of a validation state and the tool tip to
+ * display on the validated component.
  *
  * @author Randall Wood
  */
@@ -27,38 +29,116 @@ public class Validation {
     private final Type type;
     private final String message;
     private final JInputValidatorPreferences preferences;
-    
+
+    /**
+     * The Validation state. The states {@link Type#DANGER} and
+     * {@link Type#WARNING} are considered invalid in
+     * {@link JInputValidator#verify(javax.swing.JComponent)} and all other
+     * states are considered valid.
+     */
     public enum Type {
+        /**
+         * Default state with no validation icon or tool tip displayed. If the
+         * component being validated has a default tool tip, that tool tip is
+         * displayed.
+         */
         NONE,
+        /**
+         * Validation state with an unknown icon and tool tip. By default the
+         * icon is a question mark in a circle in the default border color.
+         * Considered informative and valid.
+         */
         UNKNOWN,
+        /**
+         * Validation state with an information icon and tool tip. By default
+         * the icon is a letter I in a circle in the default border color.
+         * Considered informative and valid.
+         */
         INFORMATION,
+        /**
+         * Validation state with a success icon and tool tip. By default the
+         * icon is a check mark in a circle in green. Considered informative and
+         * valid.
+         */
         SUCCESS,
+        /**
+         * Validation state with an warning icon and tool tip. By default the
+         * icon is an exclamation mark in a triangle in yellow. Considered
+         * invalid.
+         */
         WARNING,
+        /**
+         * Validation state with an danger icon and tool tip. By default the
+         * icon is a letter X in a circle in red. Considered invalid.
+         */
         DANGER,
     }
-    
+
+    /**
+     * Create a validation with default preferences.
+     *
+     * @param type    the type of validation
+     * @param message the tool tip text
+     */
+    public Validation(Type type, String message) {
+        this(type, message, JInputValidatorPreferences.getPreferences());
+    }
+
+    /**
+     * Create a validation with custom preferences.
+     *
+     * @param type        the type of validation
+     * @param message     the tool tip text
+     * @param preferences the preferences to use
+     */
     public Validation(Type type, String message, JInputValidatorPreferences preferences) {
         this.type = type;
         this.message = message;
         this.preferences = preferences;
     }
-    
-    protected Type getType() {
+
+    /**
+     * Get the validation type.
+     *
+     * @return the validation type
+     */
+    public Type getType() {
         return type;
     }
-    
-    protected String getMessage() {
+
+    /**
+     * Get the validation message used in the tool tip for the validated
+     * component.
+     *
+     * @return the tool tip text
+     */
+    public String getMessage() {
         return message;
     }
 
+    /**
+     * Get the Unicode characters for the displayed icon.
+     *
+     * @return the icon text
+     */
     public String getIcon() {
         return preferences.getIcon(type);
     }
 
+    /**
+     * Get the color the icon is displayed in.
+     *
+     * @return the color to use
+     */
     public Color getColor() {
         return preferences.getColor(type);
     }
-    
+
+    /**
+     * Get the font used to render the icon.
+     *
+     * @return the font to use
+     */
     public Font getFont() {
         return preferences.getFont();
     }
