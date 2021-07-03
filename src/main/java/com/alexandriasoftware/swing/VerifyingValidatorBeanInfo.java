90fd330d8a7748ad64050c3bb702ec5ad81e7ae6
Randall Wood <297232+rhwood@users.noreply.github.com>
2021-07-03 13:26:02 -0400

    chore: linting and formatting

--- src/main/java/com/alexandriasoftware/swing/VerifyingValidatorBeanInfo.java
+++ src/main/java/com/alexandriasoftware/swing/VerifyingValidatorBeanInfo.java
@@ -24,59 +24,64 @@ import java.beans.PropertyDescriptor;
 /**
  * Provide a graphical editor for {@link VerifyingValidator}.
  * <p>
- * Icon is from <a href="https://fontawesome.com/icons/clipboard-check?style=solid">Font Awesome</a>.
+ * Icon is from
+ * <a href="https://fontawesome.com/icons/clipboard-check?style=solid">Font
+ * Awesome</a>.
  * 
  * @author Randall Wood
  */
 @SuppressWarnings("java:S115") // static names include names of methods
 public class VerifyingValidatorBeanInfo extends AbstractValidatorBeanInfo {
 
-    // Bean descriptor//GEN-FIRST:BeanDescriptor
-    /*lazy BeanDescriptor*/
-    private static BeanDescriptor getBdescriptor(){
-        BeanDescriptor beanDescriptor = new BeanDescriptor  ( com.alexandriasoftware.swing.VerifyingValidator.class , null ); // NOI18N//GEN-HEADEREND:BeanDescriptor
-        // Here you can add code for customizing the BeanDescriptor.
+    // Bean descriptor
+    /* lazy BeanDescriptor */
+    private static BeanDescriptor getBdescriptor() {
+        return new BeanDescriptor(com.alexandriasoftware.swing.VerifyingValidator.class, null);
+    }
 
-        return beanDescriptor;     }//GEN-LAST:BeanDescriptor
-
-
-    // Property identifiers//GEN-FIRST:Properties
+    // Property identifiers
     private static final int PROPERTY_propertyChangeListeners = 0;
     private static final int PROPERTY_toolTipText = 1;
     private static final int PROPERTY_validation = 2;
 
-    // Property array 
-    /*lazy PropertyDescriptor*/
-    private static PropertyDescriptor[] getPdescriptor(){
+    // Property array
+    /* lazy PropertyDescriptor */
+    private static PropertyDescriptor[] getPdescriptor() {
         PropertyDescriptor[] properties = new PropertyDescriptor[3];
-    
+
         try {
-            properties[PROPERTY_propertyChangeListeners] = new PropertyDescriptor ( "propertyChangeListeners", com.alexandriasoftware.swing.VerifyingValidator.class, "getPropertyChangeListeners", null ); // NOI18N
-            properties[PROPERTY_toolTipText] = new PropertyDescriptor ( "toolTipText", com.alexandriasoftware.swing.VerifyingValidator.class, "getToolTipText", "setToolTipText" ); // NOI18N
-            properties[PROPERTY_validation] = new PropertyDescriptor ( "validation", com.alexandriasoftware.swing.VerifyingValidator.class, "getValidation", null ); // NOI18N
+            properties[PROPERTY_propertyChangeListeners] = new PropertyDescriptor("propertyChangeListeners",
+                    com.alexandriasoftware.swing.VerifyingValidator.class, "getPropertyChangeListeners", null);
+            properties[PROPERTY_toolTipText] = new PropertyDescriptor("toolTipText",
+                    com.alexandriasoftware.swing.VerifyingValidator.class, "getToolTipText", "setToolTipText");
+            properties[PROPERTY_validation] = new PropertyDescriptor("validation",
+                    com.alexandriasoftware.swing.VerifyingValidator.class, "getValidation", null);
+        } catch (IntrospectionException e) {
         }
-        catch(IntrospectionException e) {}//GEN-HEADEREND:Properties
-        // Here you can add code for customizing the properties array.
 
-        return properties;     }//GEN-LAST:Properties
+        return properties;
+    }
 
-    // EventSet identifiers//GEN-FIRST:Events
+    // EventSet identifiers
     private static final int EVENT_propertyChangeListener = 0;
 
     // EventSet array
-    /*lazy EventSetDescriptor*/
-    private static EventSetDescriptor[] getEdescriptor(){
+    /* lazy EventSetDescriptor */
+    private static EventSetDescriptor[] getEdescriptor() {
         EventSetDescriptor[] eventSets = new EventSetDescriptor[1];
-    
+
         try {
-            eventSets[EVENT_propertyChangeListener] = new EventSetDescriptor ( com.alexandriasoftware.swing.VerifyingValidator.class, "propertyChangeListener", java.beans.PropertyChangeListener.class, new String[] {"propertyChange"}, "addPropertyChangeListener", "removePropertyChangeListener" ); // NOI18N
+            eventSets[EVENT_propertyChangeListener] = new EventSetDescriptor(
+                    com.alexandriasoftware.swing.VerifyingValidator.class, "propertyChangeListener",
+                    java.beans.PropertyChangeListener.class, new String[] { "propertyChange" },
+                    "addPropertyChangeListener", "removePropertyChangeListener");
+        } catch (IntrospectionException e) {
         }
-        catch(IntrospectionException e) {}//GEN-HEADEREND:Events
-        // Here you can add code for customizing the event sets array.
 
-        return eventSets;     }//GEN-LAST:Events
+        return eventSets;
+    }
 
-    // Method identifiers//GEN-FIRST:Methods
+    // Method identifiers
     private static final int METHOD_addPropertyChangeListener0 = 0;
     private static final int METHOD_getPropertyChangeListeners1 = 1;
     private static final int METHOD_removePropertyChangeListener2 = 2;
@@ -85,46 +90,51 @@ public class VerifyingValidatorBeanInfo extends AbstractValidatorBeanInfo {
     private static final int METHOD_verify5 = 5;
     private static final int METHOD_verifyTarget6 = 6;
 
-    // Method array 
-    /*lazy MethodDescriptor*/
-    private static MethodDescriptor[] getMdescriptor(){
+    // Method array
+    /* lazy MethodDescriptor */
+    private static MethodDescriptor[] getMdescriptor() {
         MethodDescriptor[] methods = new MethodDescriptor[7];
-    
+
         try {
-            methods[METHOD_addPropertyChangeListener0] = new MethodDescriptor(com.alexandriasoftware.swing.JInputValidator.class.getMethod("addPropertyChangeListener", new Class[] {java.lang.String.class, java.beans.PropertyChangeListener.class})); // NOI18N
-            methods[METHOD_addPropertyChangeListener0].setDisplayName ( "" );
-            methods[METHOD_getPropertyChangeListeners1] = new MethodDescriptor(com.alexandriasoftware.swing.JInputValidator.class.getMethod("getPropertyChangeListeners", new Class[] {java.lang.String.class})); // NOI18N
-            methods[METHOD_getPropertyChangeListeners1].setDisplayName ( "" );
-            methods[METHOD_removePropertyChangeListener2] = new MethodDescriptor(com.alexandriasoftware.swing.JInputValidator.class.getMethod("removePropertyChangeListener", new Class[] {java.lang.String.class, java.beans.PropertyChangeListener.class})); // NOI18N
-            methods[METHOD_removePropertyChangeListener2].setDisplayName ( "" );
-            methods[METHOD_shouldYieldFocus3] = new MethodDescriptor(javax.swing.InputVerifier.class.getMethod("shouldYieldFocus", new Class[] {javax.swing.JComponent.class})); // NOI18N
-            methods[METHOD_shouldYieldFocus3].setDisplayName ( "" );
-            methods[METHOD_shouldYieldFocus4] = new MethodDescriptor(javax.swing.InputVerifier.class.getMethod("shouldYieldFocus", new Class[] {javax.swing.JComponent.class, javax.swing.JComponent.class})); // NOI18N
-            methods[METHOD_shouldYieldFocus4].setDisplayName ( "" );
-            methods[METHOD_verify5] = new MethodDescriptor(com.alexandriasoftware.swing.JInputValidator.class.getMethod("verify", new Class[] {javax.swing.JComponent.class})); // NOI18N
-            methods[METHOD_verify5].setDisplayName ( "" );
-            methods[METHOD_verifyTarget6] = new MethodDescriptor(javax.swing.InputVerifier.class.getMethod("verifyTarget", new Class[] {javax.swing.JComponent.class})); // NOI18N
-            methods[METHOD_verifyTarget6].setDisplayName ( "" );
+            methods[METHOD_addPropertyChangeListener0] = new MethodDescriptor(
+                    com.alexandriasoftware.swing.JInputValidator.class.getMethod("addPropertyChangeListener",
+                            java.lang.String.class, java.beans.PropertyChangeListener.class));
+            methods[METHOD_addPropertyChangeListener0].setDisplayName("");
+            methods[METHOD_getPropertyChangeListeners1] = new MethodDescriptor(
+                    com.alexandriasoftware.swing.JInputValidator.class.getMethod("getPropertyChangeListeners",
+                            java.lang.String.class));
+            methods[METHOD_getPropertyChangeListeners1].setDisplayName("");
+            methods[METHOD_removePropertyChangeListener2] = new MethodDescriptor(
+                    com.alexandriasoftware.swing.JInputValidator.class.getMethod("removePropertyChangeListener",
+                            java.lang.String.class, java.beans.PropertyChangeListener.class));
+            methods[METHOD_removePropertyChangeListener2].setDisplayName("");
+            methods[METHOD_shouldYieldFocus3] = new MethodDescriptor(
+                    javax.swing.InputVerifier.class.getMethod("shouldYieldFocus", javax.swing.JComponent.class));
+            methods[METHOD_shouldYieldFocus3].setDisplayName("");
+            methods[METHOD_shouldYieldFocus4] = new MethodDescriptor(javax.swing.InputVerifier.class
+                    .getMethod("shouldYieldFocus", javax.swing.JComponent.class, javax.swing.JComponent.class));
+            methods[METHOD_shouldYieldFocus4].setDisplayName("");
+            methods[METHOD_verify5] = new MethodDescriptor(com.alexandriasoftware.swing.JInputValidator.class
+                    .getMethod("verify", javax.swing.JComponent.class));
+            methods[METHOD_verify5].setDisplayName("");
+            methods[METHOD_verifyTarget6] = new MethodDescriptor(
+                    javax.swing.InputVerifier.class.getMethod("verifyTarget", javax.swing.JComponent.class));
+            methods[METHOD_verifyTarget6].setDisplayName("");
+        } catch (Exception e) {
         }
-        catch( Exception e) {}//GEN-HEADEREND:Methods
-        // Here you can add code for customizing the methods array.
 
-        return methods;     }//GEN-LAST:Methods
+        return methods;
+    }
 
-    private static final int defaultPropertyIndex = -1;//GEN-BEGIN:Idx
-    private static final int defaultEventIndex = -1;//GEN-END:Idx
+    private static final int defaultPropertyIndex = -1;
+    private static final int defaultEventIndex = -1;
 
-
-//GEN-FIRST:Superclass
-    // Here you can add code for customizing the Superclass BeanInfo.
-
-//GEN-LAST:Superclass
     /**
      * Gets the bean's <code>BeanDescriptor</code>s.
      *
-     * @return BeanDescriptor describing the editable properties of this bean.
-     *         May return null if the information should be obtained by
-     *         automatic analysis.
+     * @return BeanDescriptor describing the editable properties of this bean. May
+     *         return null if the information should be obtained by automatic
+     *         analysis.
      */
     @Override
     public BeanDescriptor getBeanDescriptor() {
@@ -134,14 +144,15 @@ public class VerifyingValidatorBeanInfo extends AbstractValidatorBeanInfo {
     /**
      * Gets the bean's <code>PropertyDescriptor</code>s.
      *
-     * @return An array of PropertyDescriptors describing the editable
-     *         properties supported by this bean. May return null if the
-     *         information should be obtained by automatic analysis.
-     * <p>
-     * If a property is indexed, then its entry in the result array will belong
-     * to the IndexedPropertyDescriptor subclass of PropertyDescriptor. A client
-     * of getPropertyDescriptors can use "instanceof" to check if a given
-     * PropertyDescriptor is an IndexedPropertyDescriptor.
+     * @return An array of PropertyDescriptors describing the editable properties
+     *         supported by this bean. May return null if the information should be
+     *         obtained by automatic analysis.
+     *         <p>
+     *         If a property is indexed, then its entry in the result array will
+     *         belong to the IndexedPropertyDescriptor subclass of
+     *         PropertyDescriptor. A client of getPropertyDescriptors can use
+     *         "instanceof" to check if a given PropertyDescriptor is an
+     *         IndexedPropertyDescriptor.
      */
     @Override
     public PropertyDescriptor[] getPropertyDescriptors() {
@@ -151,9 +162,9 @@ public class VerifyingValidatorBeanInfo extends AbstractValidatorBeanInfo {
     /**
      * Gets the bean's <code>EventSetDescriptor</code>s.
      *
-     * @return An array of EventSetDescriptors describing the kinds of events
-     *         fired by this bean. May return null if the information should be
-     *         obtained by automatic analysis.
+     * @return An array of EventSetDescriptors describing the kinds of events fired
+     *         by this bean. May return null if the information should be obtained
+     *         by automatic analysis.
      */
     @Override
     public EventSetDescriptor[] getEventSetDescriptors() {
@@ -163,9 +174,9 @@ public class VerifyingValidatorBeanInfo extends AbstractValidatorBeanInfo {
     /**
      * Gets the bean's <code>MethodDescriptor</code>s.
      *
-     * @return An array of MethodDescriptors describing the methods implemented
-     *         by this bean. May return null if the information should be
-     *         obtained by automatic analysis.
+     * @return An array of MethodDescriptors describing the methods implemented by
+     *         this bean. May return null if the information should be obtained by
+     *         automatic analysis.
      */
     @Override
     public MethodDescriptor[] getMethodDescriptors() {
@@ -173,14 +184,14 @@ public class VerifyingValidatorBeanInfo extends AbstractValidatorBeanInfo {
     }
 
     /**
-     * A bean may have a "default" property that is the property that will
-     * mostly commonly be initially chosen for update by human's who are
-     * customizing the bean.
+     * A bean may have a "default" property that is the property that will mostly
+     * commonly be initially chosen for update by human's who are customizing the
+     * bean.
      *
-     * @return Index of default property in the PropertyDescriptor array
-     *         returned by getPropertyDescriptors.
-     * <P>
-     * Returns -1 if there is no default property.
+     * @return Index of default property in the PropertyDescriptor array returned by
+     *         getPropertyDescriptors.
+     *         <P>
+     *         Returns -1 if there is no default property.
      */
     @Override
     public int getDefaultPropertyIndex() {
@@ -188,17 +199,17 @@ public class VerifyingValidatorBeanInfo extends AbstractValidatorBeanInfo {
     }
 
     /**
-     * A bean may have a "default" event that is the event that will mostly
-     * commonly be used by human's when using the bean.
+     * A bean may have a "default" event that is the event that will mostly commonly
+     * be used by human's when using the bean.
      *
-     * @return Index of default event in the EventSetDescriptor array returned
-     *         by getEventSetDescriptors.
-     * <P>
-     * Returns -1 if there is no default event.
+     * @return Index of default event in the EventSetDescriptor array returned by
+     *         getEventSetDescriptors.
+     *         <P>
+     *         Returns -1 if there is no default event.
      */
     @Override
     public int getDefaultEventIndex() {
         return defaultEventIndex;
     }
-    
+
 }
