138152a52395528af3f14c553b8ae9da437a4347
Randall Wood <297232+rhwood@users.noreply.github.com>
2021-07-03 19:02:01 -0400

    refactor: move common method to superclass

--- src/main/java/com/alexandriasoftware/swing/PredicateValidatorBeanInfo.java
+++ src/main/java/com/alexandriasoftware/swing/PredicateValidatorBeanInfo.java
@@ -16,16 +16,10 @@
 package com.alexandriasoftware.swing;
 
 import java.beans.BeanDescriptor;
-import java.beans.EventSetDescriptor;
 import java.beans.IntrospectionException;
-import java.beans.MethodDescriptor;
-import java.beans.PropertyChangeListener;
 import java.beans.PropertyDescriptor;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import javax.swing.InputVerifier;
-import javax.swing.JComponent;
 
 /**
  * Provide a graphical editor for {@link PredicateValidator}.
@@ -72,48 +66,6 @@ public class PredicateValidatorBeanInfo extends AbstractValidatorBeanInfo {
         return properties;
     }
 
-    // Method identifiers
-    private static final int METHOD_addPropertyChangeListener0 = 0;
-    private static final int METHOD_getPropertyChangeListeners1 = 1;
-    private static final int METHOD_removePropertyChangeListener2 = 2;
-    private static final int METHOD_shouldYieldFocus3 = 3;
-    private static final int METHOD_shouldYieldFocus4 = 4;
-    private static final int METHOD_verify5 = 5;
-    private static final int METHOD_verifyTarget6 = 6;
-
-    // Method array
-    /* lazy MethodDescriptor */
-    private static MethodDescriptor[] getMdescriptor() {
-        MethodDescriptor[] methods = new MethodDescriptor[7];
-
-        try {
-            methods[METHOD_addPropertyChangeListener0] = new MethodDescriptor(JInputValidator.class
-                    .getMethod("addPropertyChangeListener", String.class, PropertyChangeListener.class));
-            methods[METHOD_addPropertyChangeListener0].setDisplayName("");
-            methods[METHOD_getPropertyChangeListeners1] = new MethodDescriptor(
-                    JInputValidator.class.getMethod("getPropertyChangeListeners", String.class));
-            methods[METHOD_getPropertyChangeListeners1].setDisplayName("");
-            methods[METHOD_removePropertyChangeListener2] = new MethodDescriptor(JInputValidator.class
-                    .getMethod("removePropertyChangeListener", String.class, PropertyChangeListener.class));
-            methods[METHOD_removePropertyChangeListener2].setDisplayName("");
-            methods[METHOD_shouldYieldFocus3] = new MethodDescriptor(
-                    InputVerifier.class.getMethod("shouldYieldFocus", JComponent.class));
-            methods[METHOD_shouldYieldFocus3].setDisplayName("");
-            methods[METHOD_shouldYieldFocus4] = new MethodDescriptor(
-                    InputVerifier.class.getMethod("shouldYieldFocus", JComponent.class, JComponent.class));
-            methods[METHOD_shouldYieldFocus4].setDisplayName("");
-            methods[METHOD_verify5] = new MethodDescriptor(JInputValidator.class.getMethod("verify", JComponent.class));
-            methods[METHOD_verify5].setDisplayName("");
-            methods[METHOD_verifyTarget6] = new MethodDescriptor(
-                    InputVerifier.class.getMethod("verifyTarget", JComponent.class));
-            methods[METHOD_verifyTarget6].setDisplayName("");
-        } catch (Exception e) {
-            Logger.getLogger(JInputValidatorBeanInfo.class.getName()).log(Level.SEVERE, e.getMessage(), e);
-        }
-
-        return methods;
-    }
-
     /**
      * Gets the bean's <code>BeanDescriptor</code>s.
      *
@@ -142,18 +94,6 @@ public class PredicateValidatorBeanInfo extends AbstractValidatorBeanInfo {
     @Override
     public PropertyDescriptor[] getPropertyDescriptors() {
         return getPdescriptor();
-    }
-
-    /**
-     * Gets the bean's <code>MethodDescriptor</code>s.
-     *
-     * @return An array of MethodDescriptors describing the methods implemented by
-     *         this bean. May return null if the information should be obtained by
-     *         automatic analysis.
-     */
-    @Override
-    public MethodDescriptor[] getMethodDescriptors() {
-        return getMdescriptor();
     }
 
 }
