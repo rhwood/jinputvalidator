9bdec693bda27e3f441a3f2b659e0c83d1baf532
Randall Wood <297232+rhwood@users.noreply.github.com>
2021-07-03 18:40:01 -0400

    test: add *BeanInfo tests

--- src/test/java/com/alexandriasoftware/swing/AbstractValidatorBeanInfoTest.java
+++ src/test/java/com/alexandriasoftware/swing/AbstractValidatorBeanInfoTest.java
@@ -4,23 +4,18 @@ import static java.beans.BeanInfo.ICON_COLOR_16x16;
 import static java.beans.BeanInfo.ICON_COLOR_32x32;
 import static java.beans.BeanInfo.ICON_MONO_16x16;
 import static java.beans.BeanInfo.ICON_MONO_32x32;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.beans.BeanInfo;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class AbstractValidatorBeanInfoTest {
+abstract class AbstractValidatorBeanInfoTest {
 
-    private BeanInfo info;
-
-    @BeforeEach
-    public void setUp() {
-        info = new AbstractValidatorBeanInfo();
-    }
+    protected BeanInfo info;
+    protected Class<?> trgt;
 
     @Test
     void testGetIcon() {
@@ -29,5 +24,36 @@ class AbstractValidatorBeanInfoTest {
         assertNotNull(info.getIcon(ICON_MONO_16x16));
         assertNotNull(info.getIcon(ICON_MONO_32x32));
         assertNull(info.getIcon(Integer.MAX_VALUE));
+    }
+
+    @Test
+    void TestGetBeanDescriptor() {
+        assertNotNull(info.getBeanDescriptor());
+        assertEquals(trgt, info.getBeanDescriptor().getBeanClass());
+    }
+
+    @Test
+    void TestGetPropertyDescriptors() {
+        assertNotNull(info.getPropertyDescriptors());
+    }
+
+    @Test
+    void TestGetEventSetDescriptors() {
+        assertNotNull(info.getEventSetDescriptors());
+    }
+
+    @Test
+    void TestGetMethodDescriptors() {
+        assertNotNull(info.getMethodDescriptors());
+    }
+
+    @Test
+    void TestGetDefaultPropertyIndex() {
+        assertEquals(-1, info.getDefaultPropertyIndex());
+    }
+
+    @Test
+    void TestGetDefaultEventIndex() {
+        assertEquals(-1, info.getDefaultEventIndex());
     }
 }
