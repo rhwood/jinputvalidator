f677ad4d377b2dc197f112cfca78c3d78d0f502d
Randall Wood <297232+rhwood@users.noreply.github.com>
2021-09-22 09:13:12 -0400

    fix: Fix typo preventing use of saved default preference (#88)
    * fix: Fix typo preventing use of saved default preference
    
    * chore: Address checkstyle concerns

--- src/main/java/com/alexandriasoftware/swing/JInputValidatorPreferences.java
+++ src/main/java/com/alexandriasoftware/swing/JInputValidatorPreferences.java
@@ -63,6 +63,9 @@ import org.slf4j.LoggerFactory;
  */
 public class JInputValidatorPreferences {
 
+    /**
+     * The default preferences instance.
+     */
     private static JInputValidatorPreferences defaultPreferences = null;
 
     /**
@@ -87,48 +90,100 @@ public class JInputValidatorPreferences {
      * @param preferences the preferences to use
      * @return the preferences for a specific scenario
      */
-    public static JInputValidatorPreferences getPreferences(Preferences preferences) {
+    public static JInputValidatorPreferences getPreferences(final Preferences preferences) {
         return new JInputValidatorPreferences(preferences);
     }
 
+    /**
+     * The font used to display validation icons.
+     */
     private final Font font;
+    /**
+     * The icon for {@link Validation.Type#UNKNOWN}.
+     */
     private final String unknownIcon;
+    /**
+     * The color for {@link Validation.Type#UNKNOWN}.
+     */
     private final Color unknownColor;
+    /**
+     * The icon for {@link Validation.Type#INFORMATION}.
+     */
     private final String informationIcon;
+    /**
+     * The color for {@link Validation.Type#INFORMATION}.
+     */
     private final Color informationColor;
+    /**
+     * The icon for {@link Validation.Type#SUCCESS}.
+     */
     private final String successIcon;
+    /**
+     * The color for {@link Validation.Type#SUCCESS}.
+     */
     private final Color successColor;
+    /**
+     * The icon for {@link Validation.Type#WARNING}.
+     */
     private final String warningIcon;
+    /**
+     * The color for {@link Validation.Type#WARNING}.
+     */
     private final Color warningColor;
+    /**
+     * The icon for {@link Validation.Type#DANGER}.
+     */
     private final String dangerIcon;
+    /**
+     * The color for {@link Validation.Type#DANGER}.
+     */
     private final Color dangerColor;
+    /**
+     * Default color for {@link Validation.Type#DANGER} as HTML color code.
+     */
+    public static final int DEFAULT_DANGER_COLOR = 0xC9190B;
+    /**
+     * Default color for {@link Validation.Type#INFORMATION} as HTML color code.
+     */
+    public static final int DEFAULT_INFORMATION_COLOR = 0x73BCF7;
+    /**
+     * Default color for {@link Validation.Type#SUCCESS} as HTML color code.
+     */
+    public static final int DEFAULT_SUCCESS_COLOR = 0x92D400;
+    /**
+     * Default color for {@link Validation.Type#UNKNOWN} as HTML color code.
+     */
+    public static final int DEFAULT_UNKNOWN_COLOR = 0x73BCF7;
+    /**
+     * Default color for {@link Validation.Type#WARNING} as HTML color code.
+     */
+    public static final int DEFAULT_WARNING_COLOR = 0xF0AB00;
 
     private JInputValidatorPreferences() {
         this(Preferences.userNodeForPackage(JInputValidatorPreferences.class));
     }
 
-    private JInputValidatorPreferences(Preferences preferences) {
+    private JInputValidatorPreferences(final Preferences preferences) {
         Preferences defaults = Preferences.userNodeForPackage(JInputValidatorPreferences.class);
         String fontAwesome = "/com/fontawesome/Font Awesome 5 Free-Solid-900.otf";
         Font f;
         try {
             f = Font.createFont(Font.TRUETYPE_FONT, JInputValidatorPreferences.class.getResourceAsStream(preferences.get("font", defaults.get("font", fontAwesome))));
-        }
-        catch (FontFormatException | IOException ex) {
+        } catch (FontFormatException | IOException ex) {
             LoggerFactory.getLogger(this.getClass()).error("Unable to get Font resource named {}", preferences.get("font", fontAwesome), ex);
             f = (new JLabel()).getFont();
         }
         font = f;
         unknownIcon = preferences.get("unknown.icon", defaults.get("unknown.icon", "\uf059"));
-        unknownColor = new Color(preferences.getInt("unknown.color", defaults.getInt("inknown.color", 0x73BCF7)));
+        unknownColor = new Color(preferences.getInt("unknown.color", defaults.getInt("unknown.color", DEFAULT_UNKNOWN_COLOR)));
         informationIcon = preferences.get("information.icon", defaults.get("information.icon", "\uf05a"));
-        informationColor = new Color(preferences.getInt("information.color", defaults.getInt("information.color", 0x73BCF7)));
+        informationColor = new Color(preferences.getInt("information.color", defaults.getInt("information.color", DEFAULT_INFORMATION_COLOR)));
         successIcon = preferences.get("success.icon", defaults.get("success.icon", "\uf058"));
-        successColor = new Color(preferences.getInt("success.color", defaults.getInt("success.color", 0x92D400)));
+        successColor = new Color(preferences.getInt("success.color", defaults.getInt("success.color", DEFAULT_SUCCESS_COLOR)));
         warningIcon = preferences.get("warning.icon", defaults.get("warning.icon", "\uf071"));
-        warningColor = new Color(preferences.getInt("warning.color", defaults.getInt("warning.color", 0xF0AB00)));
+        warningColor = new Color(preferences.getInt("warning.color", defaults.getInt("warning.color", DEFAULT_WARNING_COLOR)));
         dangerIcon = preferences.get("danger.icon", defaults.get("danger.icon", "\uf06a"));
-        dangerColor = new Color(preferences.getInt("danger.color", defaults.getInt("danger.color", 0xC9190B)));
+        dangerColor = new Color(preferences.getInt("danger.color", defaults.getInt("danger.color", DEFAULT_DANGER_COLOR)));
     }
 
     /**
@@ -149,7 +204,7 @@ public class JInputValidatorPreferences {
      * @return the specified icon or an empty String if type is
      *         {@link Validation.Type#NONE} or not recognized
      */
-    public String getIcon(Validation.Type type) {
+    public String getIcon(final Validation.Type type) {
         switch (type) {
             case UNKNOWN:
                 return unknownIcon;
@@ -175,7 +230,7 @@ public class JInputValidatorPreferences {
      * @return the specified color or {@link java.awt.SystemColor#textText} if
      *         type is {@link Validation.Type#NONE} or not recognized
      */
-    public Color getColor(Validation.Type type) {
+    public Color getColor(final Validation.Type type) {
         switch (type) {
             case UNKNOWN:
                 return unknownColor;
